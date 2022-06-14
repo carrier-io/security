9d53451774b1852afd21223750a59dd19fd9b521
Aspect13 <ad13box@gmail.com>
2022-06-14 14:20:13 +0200

    Hotfix/results page (#31)

--- static/js/results/artifacts.js
+++ static/js/results/artifacts.js
@@ -1,29 +1,11 @@
-// page_params = page_params || new URLSearchParams(location.search);
-
-const getTableUrlArtifacts = () => {
-    const result_test_id = new URLSearchParams(location.search).get('result_id')
-    return `/api/v1/artifacts/security_results/${result_test_id}/`
-}
-const getTableUrlDownloadArtifacts = () => {
-    const result_test_id = new URLSearchParams(location.search).get('result_id')
-    return `/api/v1/artifacts/security_download/${result_test_id}`
-}
-
-function renderTableArtifacts() {
-    $("#artifacts").bootstrapTable('refresh', {
-        url: getTableUrlArtifacts(),
-    })
-}
+const artifact_download_url = `/api/v1/artifacts/security_download/${new URLSearchParams(location.search).get('result_id')}`
 
 function artifactActionsFormatter(value, row, index) {
     return `
         <a 
-            href="${getTableUrlDownloadArtifacts()}/${row['name']}" 
+            href="${artifact_download_url}/${row['name']}" 
             class="fa fa-download btn-action" 
             download="${row['name']}"
         ></a>
     `
 }
-
-
-$(document).on('vue_init', () => $(renderTableArtifacts()))
