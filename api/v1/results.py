ec9d6858263fa3f2de9ab3224428a0df2feb0c6e
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-07-18 12:13:06 +0500

    Feature: Test Results delete

--- api/v1/results.py
+++ api/v1/results.py
@@ -27,3 +27,16 @@ class API(Resource):
         for each in res:
             reports.append(each.to_json())
         return make_response({"total": total, "rows": reports}, 200)
+
+    def delete(self, project_id: int):
+        project = self.module.context.rpc_manager.call.project_get_or_404(project_id=project_id)
+        try:
+            delete_ids = list(map(int, request.args["id[]"].split(',')))
+        except TypeError:
+            return make_response('IDs must be integers', 400)
+        SecurityResultsDAST.query.filter(
+            SecurityResultsDAST.project_id == project.id,
+            SecurityResultsDAST.id.in_(delete_ids)
+        ).delete()
+        SecurityResultsDAST.commit()
+        return {"message": "deleted"}, 204
